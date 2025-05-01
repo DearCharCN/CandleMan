680337694e6b360fbd9eb4c89593353e5d63d24b
底潮 <dearchar@qq.com>
2025-05-01 20:22:21 +0800

    提交

---
+++ Assets/Scripts/GamePlay/CandleBody.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleBody : MonoBehaviour
+{
+    [SerializeField]
+    float length;
+    [SerializeField]
+    float unitHeight;
+    [SerializeField]
+    RectTransform rootTrans;
+}
